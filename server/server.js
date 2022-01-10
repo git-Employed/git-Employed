b49a09681ed877c4db9fb449abf404d5abc48137
lamerson28 <82220715+rozay28@users.noreply.github.com>
2022-01-10 12:24:42 -0600

    Boilerplate webpack added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, '../client')));
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
