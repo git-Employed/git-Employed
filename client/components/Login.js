811c7713ecc5fa876361982905bb7620f9688185
lamerson28 <82220715+rozay28@users.noreply.github.com>
2022-01-10 16:35:23 -0600

    In process of incorporating passport.js's github oauth strategy

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,7 +1,21 @@
-import React from "react"
+import React from "react";
 
 export default function Login() {
+  const getToken = () => {
+    const request = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    };
+    fetch("https://github.com/login/oauth/authorize", request)
+      .then
+  };
+
   return (
-    <h1>hellooooo</h1>
-  )
+    <div>
+      <h1>Sign Into GitEmployed</h1>
+      <button onClick={getToken}>Sign In with GitHub</button>
+    </div>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -2,11 +2,26 @@ const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const passport = require('passport');
+
+// Configure github passport strategy
+passport.use( new GitHubStrategy ({
+  clientID: '685f0e0a21587f473ca1',
+  clientSecret: '546bd6ecdc0902be03cb6b3d57f282e3ca2ceb19'
+})
+
+)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, '../client')));
+
+
+app.get('/auth/github',
+  passport.authenticate
+)
+
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
