3366ad1f47a5695701d35eef5ec184c86394b3b3
lamerson28 <82220715+rozay28@users.noreply.github.com>
2022-01-11 17:09:54 -0600

    Working oauth with github

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const app = express();
 const passport = require("passport");
 const GitHubStrategy = require("passport-github2").Strategy;
 const { userInfo } = require("os");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../client")));
+app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
@@ -21,50 +26,54 @@ passport.use(
       // Note: add the client secret and client id to .env file
       clientID: "685f0e0a21587f473ca1",
       clientSecret: "facb1fb26ecd809696b690502f1894765cae8fa0",
-      callbackURL: "http://localhost:8080/auth/github/callback"
+      callbackURL: "http://localhost:8080/auth/github/callback",
     },
     function(accessToken, refreshToken, profile, done) {
-      User.findOrCreate({ githubId: profile.id }, function (err, user) {
-        return done(err, user);
+      // asynchronous verification, for effect...
+      process.nextTick(function () {
+        
+        // To keep the example simple, the user's GitHub profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the GitHub account with a user record in your database,
+        // and return that user instead.
+        console.log(profile);
+        return done(null, profile);
       });
     }
-  )
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, "../client")));
-app.use(passport.initialize());
-// app.use(passport.session());
+  ));
 
 // endpoint for github oauth request
 app.get(
-  "/auth/github", (res, req, next) => {
-    console.log('ENTERING AUTHENTICATION')
+  "/auth/github",
+  (res, req, next) => {
+    console.log("ENTERING AUTHENTICATION");
     next();
   },
-  passport.authenticate('github', { scope: ['read:user'] })
+  passport.authenticate("github", { scope: ["read:user"] })
 );
 
 //endpoint that github sends a successful auth back to
 app.get(
   "/auth/github/callback",
   (res, req, next) => {
-    console.log('ENTERING CALLBACK');
+    console.log("ENTERING CALLBACK");
     next();
   },
-  passport.authenticate('github', { failureRedirect: "/auth/github/error" }),
+  passport.authenticate("github", { failureRedirect: "/auth/github/error" }),
   (req, res) => {
     // If the redirect was successful, redirect to homepage
     // May be part of the problem here, could need to be another page
-    // res.redirect("/");
+    res.redirect("/auth/github/success");
   }
 );
 
-app.use('/auth/github/error', (req, res) => {
-  res.send('Sending to this error endpoint')
+app.use("/auth/github/error", (req, res) => {
+  res.send("Sending to this error endpoint");
 });
 
+app.use("/auth/github/success", (req, res) => {
+  res.send("Success!");
+});
 
 // catch-all route handler for any requests to an unknown route
 app.use("*", (req, res) => {
