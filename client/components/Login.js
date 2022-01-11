2793916eb66ac95b3a8d553ddb8ddfd84ae67481
lamerson28 <82220715+rozay28@users.noreply.github.com>
2022-01-11 14:03:24 -0600

    OAuth middleware added, getting unknown middleware error in the callback route

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 export default function Login() {
-  const getToken = () => {
-    const request = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    };
-    fetch('/auth/github', request)
-      .then
-  };
+  // const getToken = () => {
+  //   const request = {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     }
+  //   };
+  //   fetch('/auth/github', request)
+  //     .then
+  // };
 
   return (
     <div>
       <h1>Sign Into GitEmployed</h1>
-      <button onClick={getToken}>Sign In with GitHub</button>
+      <button>
+        <a href="/auth/github">Login with GitHub</a>
+      </button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -3,18 +3,27 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
 const { userInfo } = require("os");
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // Configure github passport strategy
 passport.use(
   new GitHubStrategy(
     {
       // Note: add the client secret and client id to .env file
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost8080/homepage",
+      clientID: "685f0e0a21587f473ca1",
+      clientSecret: "facb1fb26ecd809696b690502f1894765cae8fa0",
+      callbackURL: "http://10.0.0.59:8080/auth/github/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
+    function(accessToken, refreshToken, profile, done) {
       User.findOrCreate({ githubId: profile.id }, function (err, user) {
         return done(err, user);
       });
@@ -26,26 +35,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "../client")));
 app.use(passport.initialize());
-
+// app.use(passport.session());
 
 // endpoint for github oauth request
 app.get(
-  "/auth/github",
-  passport.authenticate("github", { scope: ["user:email"] })
-);
-
-//endpoint that github sends a successful auth back to 
-app.get(
-  "/auth/github/callback",
-  passport.authenticate("github", { failureRedirect: "/" }),
-  (req, res) => {
-    // If the redirect was successful, redirect to homepage
-    res.redirect("/homepage")
+  "/auth/github", (res, req, next) => {
+    console.log('ENTERING AUTHENTICATION')
+    next();
+  },
+  passport.authenticate('github', { scope: ['read:user'] }),
+  // According to example linked from passport docs, this will never be called
+  (res, req) => {
+    console.log('EXITING AUTHENTICATION')
   }
 );
 
+//endpoint that github sends a successful auth back to
+app.get(
+  "/auth/github/callback",
+  (res, req, next) => {
+    console.log('ENTERING CALLBACK');
+    next();
+  },
+  passport.authenticate('github', { failureRedirect: "/auth/github/error" }),
+  (req, res) => {
+    // If the redirect was successful, redirect to homepage
+    res.redirect("/");
+  }
+);
+
+app.use('/auth/github/error', (req, res) => {
+  res.send('Sending to this error endpoint')
+});
+
+
 // catch-all route handler for any requests to an unknown route
-app.use('*',(req, res)=>{
+app.use("*", (req, res) => {
   res.sendStatus(404);
 });
 
@@ -53,18 +78,17 @@ app.use('*',(req, res)=>{
  * configire express global error handler
  * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
  */
-app.use((err,req,res,next)=> {
+app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred' } 
+    message: { err: "An error occurred" },
   };
 
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
 
   res.status(errorObj.status).json(errorObj.message);
-
 });
 
 app.listen(port, () => {
