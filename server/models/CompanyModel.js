1fc1866fa72472775e40874f8cdf0f1b4ebf5a1e
AllieMarzieh <alliegramy1993@gmail.com>
2022-01-11 11:44:14 -0500

    created a get requests for all cards

--- server/models/CompanyModel.js
+++ server/models/CompanyModel.js
@@ -3,12 +3,10 @@ dotenv.config();
 const { Pool } = require("pg");
 
 const myURI = process.env.URI;
-
-//process.env.PG_URI to falsey
 const URI = process.env.PG_URI || myURI;
-// creating a new instance of a pool to
-// we are creating a new object
-const pool = new Pool({ connectionString: URI });
+
+// creating a new instance of a pool
+const pool = new Pool({ connectionString: myURI });
 
 //export model
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -13,9 +13,8 @@ app.get("/", (req, res) => {
 	res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
 });
 app.get("/cards", cardController.getCards, (req, res) => {
-	console.log();
 	console.log(res.locals.cards);
-	return res.status(200).send(res.locals.cards);
+	return res.status(200).json(res.locals.cards);
 });
 
 app.use((err, req, res, next) => {
